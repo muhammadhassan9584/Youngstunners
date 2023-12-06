7648a9d2df4b149dc4fe7227b6c071e55dccc338
youngstunners9584 <153077454+youngstunners9584@users.noreply.github.com>
2023-12-06 15:30:41 +0500

    Add files via upload
    Young Stunners are a Pakistani hip-hop duet formed in Karachi in 2012 by Talha Anjum, Talhah Yunus and with the music production by Jokhay. The Trio is composed of Talha Anjum and Talhah Yunus Jokhay. Talha Anjum and Talhah Yunus rose to fame as rappers on YouTube after releasing their first song "Burger-e-Karachi".

---
+++ file.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include"file..h"
+int main(){
+	int a,b;
+	printf("Sum of two numbers is %d", add(8,4));
+	printf("\nSubraction of two numbers is %d", sub(8,4));
+	printf("\nProduct of two numbers is %d", product(8,4));
+   printf("\nDivision of two numbers is %d", division(8,4));
+}
+
